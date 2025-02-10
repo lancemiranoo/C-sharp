5be8381c146ac1b920e0fbcffda59d4a10b7473d
lancemiranoo <lancemirano01@gmail.com>
2025-02-10 07:51:43 -0700

    Updated files

--- Full Stack/Week 2/MovieList/MovieList/obj/Debug/net8.0/MovieList.AssemblyInfo.cs
+++ Full Stack/Week 2/MovieList/MovieList/obj/Debug/net8.0/MovieList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4026d1e78908e354641354d9be70ff284d29b49e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceadbe076b56d616ff4a1a9a00b285f4f8b82715")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
